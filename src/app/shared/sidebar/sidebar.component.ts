96125ecc1ff9897fe574b390b1e3b9b0740280f0
Macario Hernandez <mackhdez@hotmail.com>
2021-04-08 01:21:34 -0400

    gifs app

---
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { GifsService } from '../../gifs/services/gifs.service';
+import { Gif } from '../../gifs/interface/gifs.interface';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent {
+
+  get historial() {
+    return this.gifsService.historial;
+  }
+
+  constructor(private gifsService: GifsService) { }
+
+  buscar(query: string) {
+    this.gifsService.buscarGifs(query);
+  }
+}
